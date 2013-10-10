2779df45d9b9bc8b85cc498d54c8fc4e2ed110d8
Gil Forcada <gil.forcada@freitag.de>
2013-10-10 14:14:34 +0200

    Use single quotes and sort imports

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+
 import os
+
 
 version = '1.0'
 
@@ -9,21 +12,21 @@ long_description = (
     open('CONTRIBUTORS.txt').read()
     + '\n' +
     open('CHANGES.rst').read()
-    + '\n')
+)
 
 setup(name='plonesocial.messaging',
       version=version,
-      description="",
+      description='',
       long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
       keywords='',
       author='',
@@ -45,6 +48,6 @@ setup(name='plonesocial.messaging',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["templer.localcommands"],
+      setup_requires=['PasteScript'],
+      paster_plugins=['templer.localcommands'],
       )
